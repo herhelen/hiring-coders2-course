5e177aad4542cdb23af4671e63e55ce975ee3328
Helen Her <her.helen@gmail.com>
2021-07-14 00:21:06 -0300

    example of ternary operator

--- 05-js-ii/02-aula.js
+++ 05-js-ii/02-aula.js
@@ -5,3 +5,21 @@
 let variable = 'some text';
 console.log(`printing variable: ${variable}`);
 
+// Aula 03
+// Ternary / Conditional Operator
+// condition ? true : false
+
+function conditional(value) {
+    return value === 0 ? 'zero' : 'not zero';
+}
+
+console.log(`1 is ${conditional(1)}`);
+console.log(`0 is ${conditional(0)}`);
+
+let value = 0;
+let a = value === 0 ? 'zero' : 'not zero';
+console.log(a);
+
+value = 10;
+a = value === 0 ? 'zero' : 'not zero';
+console.log(a);
