e75c88fdae3484f5098b5cd0090a9f0730cf3c9a
Helen Her <her.helen@gmail.com>
2021-07-14 00:28:08 -0300

    example arrow function

--- 05-js-ii/02-aula.js
+++ 05-js-ii/02-aula.js
@@ -1,11 +1,11 @@
-// Aula 02
+// 02
 // Template String ` `
 // Interpolation in template String `text ${variable}`
 
 let variable = 'some text';
 console.log(`printing variable: ${variable}`);
 
-// Aula 03
+// 03
 // Ternary / Conditional Operator
 // condition ? true : false
 
@@ -23,3 +23,18 @@ console.log(a);
 value = 10;
 a = value === 0 ? 'zero' : 'not zero';
 console.log(a);
+
+// 04
+// Arrow Functions
+// argument => one line
+// (arguments) => { more than one }
+
+let media = (n1, n2) => (n1 + n2) / 2;
+console.log(media(10, 9));
+
+let mediaStatus = (n1, n2) => {
+    let a = (n1 + n2) / 2;
+    return a >= 7 ? 'aprovado' : 'reprovado';
+};
+console.log(mediaStatus(10, 9));
+console.log(mediaStatus(0, 1));
